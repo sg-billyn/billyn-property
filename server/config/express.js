64b9a667ca52cb9d49e20957ef21ce12736e86ac
Jianfeng <jianfeng.wang@sap.com>
2016-08-01 14:42:41 +0800

    allow cross domain authenticate

--- server/config/express.js
+++ server/config/express.js
@@ -35,11 +35,12 @@ export default function (app) {
   app.use(passport.initialize());
 
   //allow cross domain
-  app.all('/', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-  });
+  app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
+      res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Accept,Authorization");
+      next();
+    });
 
   // Persist sessions with mongoStore / sequelizeStore
   // We need to enable sessions for passport-twitter because it's an
